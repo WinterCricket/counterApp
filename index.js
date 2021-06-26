bf7aaadc5254d25e10b2fb1ca2a4a8f184fe5d00
wintercricket <hodhodhod2003@gmail.com>
2021-06-26 16:13:38 -0500

    added the save functions and refactored

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 //document.getElementById("count-el").innerText = 0;
 
 let count = 0;
-
+//let save = 0;
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-	count = count + 1;
+	count += 1;
 
 	countEl.innerText = count;
 
@@ -15,8 +16,13 @@ function increment() {
 }
 
 function decrement() {
-	count = count - 1;
+	count -= 1;
 
 	countEl.innerText = count;
 
 }
+
+function save() {
+
+	saveEl.innerText += count +"/";
+ }
