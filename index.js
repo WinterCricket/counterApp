e0e8a49dff901b60e87b686448315f5917f87519
wintercricket <hodhodhod2003@gmail.com>
2021-06-26 14:45:38 -0500

    employ dry principle in js file

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 
 
 //document.getElementById("count-el").innerText = 0;
-		
-		let count = 0;
 
-		function increment() {
-		count = count + 1;
-		
-	document.getElementById("count-el").innerText = count;
+let count = 0;
 
+let countEl = document.getElementById("count-el")
 
-		}
+function increment() {
+	count = count + 1;
 
-		function decrement() {
-		count = count - 1;
-		
-	document.getElementById("count-el").innerText = count;
+	countEl.innerText = count;
 
 
-		}
+}
+
+function decrement() {
+	count = count - 1;
+
+	countEl.innerText = count;
+
+}
