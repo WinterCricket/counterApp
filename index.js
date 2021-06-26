068f9f75432f7ba79b30a1bb22cf4e6cd2197324
wintercricket <hodhodhod2003@gmail.com>
2021-06-26 14:12:46 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+//document.getElementById("count-el").innerText = 0;
+		
+		let count = 0;
+
+		function increment() {
+		count = count + 1;
+		
+	document.getElementById("count-el").innerText = count;
+
+
+		}
+
+		function decrement() {
+		count = count - 1;
+		
+	document.getElementById("count-el").innerText = count;
+
+
+		}
