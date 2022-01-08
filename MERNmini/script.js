bf7be4319246506378f0604b37e3025028166e73
wintercricket <hodhodhod2003@gmail.com>
2022-01-08 01:08:07 -0600

    add some classes and ids and some js

---
+++ MERNmini/script.js
@@ -0,0 +1,6 @@
+const inputVal = document.getElementById("inputVal");
+const outputVal = document.querySelector("outputVal");
+let stringly = "";
+inputVal.addEventListener("input", function () {
+  stringly = stringly + stringly;
+});
